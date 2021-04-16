0bb4c4c6e3ddc5015c7502ab691ad5e0572e8d8d
Rajorshi Roy <rajorshi.me@gmail.com>
2021-04-17 00:43:25 +0530

    initial commit

---
+++ src/test/java/com/mdu/fraudmanagement/FraudmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdu.fraudmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
